4c9a0233d7861d2201286a3129d600e85b879745
ashleyzam <ashleyzam1998@gmail.com>
2022-11-29 12:25:31 -0300

    asd

--- src/controllers/workoutController.js
+++ src/controllers/workoutController.js
@@ -11,8 +11,25 @@ const getSingleWorkout = (req, res) => {
 };
 
 const createWorkout = (req, res) => {
-  const createdWorkout = workoutService.createWorkout(req.params.workoutId);
-  res.send(`Create workout ${req.params.workoutId}`);
+  const { body } = req;
+  if (
+    !body.name ||
+    !body.mode ||
+    !body.equipment ||
+    !body.excercises ||
+    !body.trainerTips
+  ) {
+    return;
+  }
+  const newWorkout = {
+    name: body.name,
+    mode: body.mode,
+    equipment: body.equipment,
+    excercises: body.excercises,
+    trainerTips: body.trainerTips,
+  };
+  const createdWorkout = workoutService.createWorkout(newWorkout);
+  res.status(201).send({ status: "OK", data: createdWorkout });
 };
 
 const updateSingleWorkout = (req, res) => {
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const v1WorkOutRouter = require("./v1/routes/workoutRoutes");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(express.json());
 app.use("/api/v1/workouts", v1WorkOutRouter);
 
 app.listen(PORT, () => {
